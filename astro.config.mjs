f4cfeaa86082e4841e7e868b2e17b4da6cb5ffd3
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-06-06 20:23:46 +0800

    Add footnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://gnoxnahte.github.io',
     // base: "portfolio-main",
+
+    // For quick access
+    devToolbar: {
+        enabled: false
+    }
 });
