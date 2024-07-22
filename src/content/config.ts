59b41bfe12be99c2786c6586b2012b0ef191ea07
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-07-22 21:06:01 +0800

    Modify landing page and other minor changes
    Landing page:
    - Set the font-size based on the screen size.
    - Add backdrop filter (blur) for webkit (iOS - Safari)
    Other minor changes:
    - Add unlisted as a property to content collections,
    - Add resume link to About page
    - Remove example content page.

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,14 +5,15 @@ const projectsCollection = defineCollection({
     schema: ({ image }) => z.object({
         title: z.string(),
         shortDescription: z.string(),
-        startDate: z.date(),
-        releaseDate: z.date().optional(),
+        startDate: z.date(), // format is YYYY-MM-DD
+        releaseDate: z.date().optional(), // format is YYYY-MM-DD
         projectLink: z.string().url().optional(),
         githubLink: z.string().url().optional(),
         rank: z.number(),
         thumbnailImg: image(),
         // TODO: Add verification
         tags: z.array(z.string()),
+        isUnlisted: z.boolean().optional(), // Page is available but won't show up in "/projects/" page;
     })
 })
 
