92eb8929e4a3f8dd069848d94b5788f8dc2cf12e
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-10-22 17:22:06 +0800

    greatly improve the project page styling
    - Add banner, subtitle options.
    - Add all the metadata for the projects to the page.
    - Improve the general styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const projectsCollection = defineCollection({
     type: 'content',
     schema: ({ image }) => z.object({
         title: z.string(),
+        subtitle: z.string().optional(),
         shortDescription: z.string(),
         startDate: z.date(), // format is YYYY-MM-DD
         releaseDate: z.date().optional(), // format is YYYY-MM-DD
@@ -11,6 +12,7 @@ const projectsCollection = defineCollection({
         githubLink: z.string().url().optional(),
         rank: z.number(),
         thumbnailImg: image(),
+        bannerImg: image().optional(),
         // TODO: Add verification
         tags: z.array(z.string()),
         isUnlisted: z.boolean().optional(), // Page is available but won't show up in "/projects/" page;
